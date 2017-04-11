d9d862407a6df03655484e568031b6fa4c5ea57b
Aura Mirea <amirea@pivotal.io>
2017-04-11 16:38:04 +0200

    Sonar build task

--- tasks/maven-build.sh
+++ tasks/maven-build.sh
@@ -3,5 +3,6 @@
 cd source-code || echo "missing input resource: source-code"
 
 echo "Using MAVEN_OPTS: ${MAVEN_OPTS}"
+echo "Using sonar at: ${SONAR_ADDRESS}"
 
-mvn verify ${MAVEN_ARGS}
+mvn sonar:sonar -Dsonar.host.url=${SONAR_ADDRESS}
